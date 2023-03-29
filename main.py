2fb1615a1b706f92230a22c6b0cfa666620cfd7f
walker-1024 <walker_walker@foxmail.com>
2023-03-29 17:45:32 +0800

    union furniture icon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 
-# 根据给定的主题ID，将这个主题里的各个家具在资源文件夹内的相关资源文件（fbx、png等）找出来，并复制输出到一个新的文件夹里，同时对资源文件用md5去重
-
 import json
 import os
 import hashlib
@@ -19,6 +17,10 @@ if not os.path.exists(outputBaseDirPath):
 buildingDirPath = os.path.join(outputBaseDirPath, "building")
 if not os.path.exists(buildingDirPath):
     os.mkdir(buildingDirPath)
+
+furnitureIconDirPath = os.path.join(outputBaseDirPath, "furniture_icon")
+if not os.path.exists(furnitureIconDirPath):
+    os.mkdir(furnitureIconDirPath)
 
 
 def getAllThemeIDs() -> list[str]:
@@ -46,7 +48,12 @@ def filterFurnitureWithTheme(themeID: str) -> Optional[list[str]]:
         print("Error: 数据文件格式不合预期")
 
 
-def unionFurniture(themeShortID: str, resourceDataDirPath: str, furnitureIDArray: list[str], outputDirPath: str):
+def unionFurniture(themeID: str, resourceDataDirPath: str, furnitureIDArray: list[str]):
+    outputDirPath = os.path.join(buildingDirPath, themeID)
+    if not os.path.exists(outputDirPath):
+        os.mkdir(outputDirPath)
+
+    themeShortID = themeID[10:]
 
     # key 为文件名，value 为 (文件md5, 文件大小)
     existFileDic = {}
@@ -100,18 +107,84 @@ def unionFurniture(themeShortID: str, resourceDataDirPath: str, furnitureIDArray
     print(f"找到含家具ID的文件夹{len(foundFurnitureIDArray)}个，找到含主题简名的png文件{len(foundSinglePngFileArray)}个。")
 
 
+def unionFurnitureIcon(themeID: str, resourceDataDirPath: str, furnitureIDArray: list[str]):
+    outputDirPath = os.path.join(furnitureIconDirPath, themeID)
+    if not os.path.exists(outputDirPath):
+        os.mkdir(outputDirPath)
+
+    furnitureIconIDArray = []
+    if "customData" in buildingDataDic and "furnitures" in buildingDataDic["customData"]:
+        for furID in furnitureIDArray:
+            if furID not in buildingDataDic["customData"]["furnitures"]:
+                print(f"Warning: 主题内的这个家具（{furID}）不在数据文件内")
+                furnitureIconIDArray.append(furID)
+            else:
+                furModel = buildingDataDic["customData"]["furnitures"][furID]
+                if "iconId" in furModel:
+                    furnitureIconIDArray.append(furModel["iconId"])
+                else:
+                    print(f"Warning: 主题内的这个家具（{furID}）数据格式不合预期-3")
+                    furnitureIconIDArray.append(furID)
+    else:
+        print("Error: 数据文件格式不合预期")
+        furnitureIconIDArray = furnitureIDArray
+
+    # key 为文件名，value 为 (文件md5, 文件大小)
+    existFileDic = {}
+    foundFurnitureIconIDArray = []
+
+    def copyFileToOutputDir(filePath: str):
+        if os.path.isdir(filePath):
+            fileList = os.listdir(filePath)
+            for item in fileList:
+                copyFileToOutputDir(os.path.join(filePath, item))
+        else:
+            _, name = os.path.split(filePath)
+            with open(filePath, "rb") as f:
+                content = f.read()
+            theMd5 = hashlib.md5(content).hexdigest().upper()
+            theSize = os.path.getsize(filePath)
+            if name in existFileDic:
+                beforeMd5, beforeSize = existFileDic[name]
+                if theMd5 != beforeMd5:
+                    if theSize > beforeSize:
+                        with open(os.path.join(outputDirPath, name), "wb") as f:
+                            f.write(content)
+                        existFileDic[name] = (theMd5, theSize)
+                    print(f"Warning: 去重失败！出现了同名但不同内容的文件（{name}），已保留较大的文件")
+            else:
+                with open(os.path.join(outputDirPath, name), "wb") as f:
+                    f.write(content)
+                existFileDic[name] = (theMd5, theSize)
+
+    def searchPath(path: str):
+        if not os.path.exists(path):
+            return
+        if os.path.isdir(path):
+            fileList = os.listdir(path)
+            for item in fileList:
+                searchPath(os.path.join(path, item))
+        else:
+            _, fileName = os.path.split(path)
+            for iconID in furnitureIconIDArray:
+                if iconID + ".png" == fileName:
+                    copyFileToOutputDir(path)
+                    foundFurnitureIconIDArray.append(iconID)
+                    break
+
+    searchPath(resourceDataDirPath)
+    print(f"找到家具图标{len(foundFurnitureIconIDArray)}个。")
+
+
 def runWithThemeID(themeID: str, resourceDataDirPath: str):
     print("=====================================")
     print(f"开始处理 {themeID}")
     if len(themeID) > 10 and len(resourceDataDirPath) > 0:
-        themeShortID = themeID[10:]
         furnitureIDArray = filterFurnitureWithTheme(themeID)
         if furnitureIDArray is not None:
             print(f"The length of furnitureIDArray is {len(furnitureIDArray)}.")
-            outputDirPath = os.path.join(buildingDirPath, themeID)
-            if not os.path.exists(outputDirPath):
-                os.mkdir(outputDirPath)
-            unionFurniture(themeShortID, resourceDataDirPath, furnitureIDArray, outputDirPath)
+            unionFurniture(themeID, resourceDataDirPath, furnitureIDArray)
+            unionFurnitureIcon(themeID, resourceDataDirPath, furnitureIDArray)
             print(f"{themeID} Done")
     else:
         print(f"Error: themeID（{themeID}）或resourceDataDirPath（{resourceDataDirPath}）非法")
